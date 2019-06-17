a777da063924d94935cecffcab4f4438887b7253
Adrian <adrian.zawadzki@outlook.com>
2019-06-17 18:43:05 +0200

    animations on scroll

--- resources/js/index.js
+++ resources/js/index.js
@@ -35,8 +35,28 @@ $(document).ready(function() {
       }
     });
   });
- 
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn')
+  }, {
+    offset: '50%',
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp')
+  }, {
+    offset: '50%',
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeIn')
+  }, {
+    offset: '50%',
+  });
 });
+
+
 
 
 
